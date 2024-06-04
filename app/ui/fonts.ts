34a66b140f39e23508a464d357957e1fe66b3866
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-06-05 00:49:34 +0900

    #Learning7 FetchingData(미리하는)

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { Inter, Lusitana, Noto_Sans_KR} from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']})
-export const lusitana = Lusitana({subsets: ['latin'], weight: ['400','700']})
-export const noto_sans_kr = Noto_Sans_KR({subsets: ['latin-ext'],weight: ['400','700']})
+export const lusitana = Lusitana({weight: ['400','700'], subsets: ['latin']})
+export const noto_sans_kr = Noto_Sans_KR({weight: ['400','700'],subsets: ['latin-ext']})
