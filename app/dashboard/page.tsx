c317bee840882c9e3cb24cd721bc08497fa2b31e
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-06-05 21:18:30 +0900

    #L7. dashboard에 revenue추가

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,15 @@
 import { Card } from "@/app/ui/dashboard/cards";
 import DashboardSkeleton from "@/app/ui/skeletons";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import { lusitana } from "../ui/fonts";
+import { fetchRevenue } from "../lib/data";
+import RevenueChart from "../ui/dashboard/revenue-chart";
 
-export default function Page() {
+export default async function Page() {
+    const revenue = await fetchRevenue();
     return (
         // <DashboardSkeleton />
-    <p>Dashboard Page</p>
+    // <p>Dashboard Page</p>
+    <RevenueChart revenue={revenue} ></RevenueChart>
     );
 }
