e6c655da79b197ddaaa8eaa394eafee70886ddf2
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-06-06 03:23:51 +0900

    #L9-2 Route Group 스켈레톤 버그 수정

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,12 +20,13 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // lazy data fetching 시 발생하는 속도 시뮬레이션 
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
